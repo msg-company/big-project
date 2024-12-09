ba2ea575607b5a3fec7254be241c9844cdd1ab74
Kyrill Dronov <kyrill.dronov@symbiosemanagement.com>
2024-12-09 01:27:42 +0100

    chore: удалены хеши коммитов из github actions

--- apps/frontend/sso/app/page.tsx
+++ apps/frontend/sso/app/page.tsx
@@ -56,16 +56,16 @@ export default function Home() {
             href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
             target="_blank"
             rel="noopener noreferrer"
-            aria-label="Deploy to Vercel platform"
           >
             <Image
               className={styles.logo}
               src="/vercel.svg"
-              alt="Vercel logomark"
+              alt=""
               width={20}
               height={20}
+              aria-hidden="true"
             />
-            Deploy now
+            <span>Deploy now</span>
           </a>
           <a
             href="https://turbo.build/repo/docs?utm_source"
@@ -73,7 +73,7 @@ export default function Home() {
             rel="noopener noreferrer"
             className={styles.secondary}
           >
-            Read our docs
+            <span>Read our docs</span>
           </a>
         </div>
       </main>
@@ -83,18 +83,16 @@ export default function Home() {
             href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-            aria-label="View Turborepo examples"
           >
-            <Image aria-hidden src="/window.svg" alt="" width={16} height={16} />
+            <Image src="/window.svg" alt="" width={16} height={16} aria-hidden="true" />
             <span>Examples</span>
           </a>
           <a
             href="https://turbo.build?utm_source=create-turbo"
             target="_blank"
             rel="noopener noreferrer"
-            aria-label="Visit Turbo build website"
           >
-            <Image aria-hidden src="/globe.svg" alt="" width={16} height={16} />
+            <Image src="/globe.svg" alt="" width={16} height={16} aria-hidden="true" />
             <span>Go to turbo.build →</span>
           </a>
         </nav>
