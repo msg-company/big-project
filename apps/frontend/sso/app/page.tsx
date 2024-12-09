065060d718e192737b5b556a098cb95aeade9f77
Kyrill Dronov <kyrill.dronov@symbiosemanagement.com>
2024-12-09 01:14:30 +0100

    feat: улучшена доступность в sso приложении

--- apps/frontend/sso/app/page.tsx
+++ apps/frontend/sso/app/page.tsx
@@ -21,7 +21,7 @@ const ThemeImage = (props: Props) => {
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
+      <nav aria-label="Main navigation">
         <ThemeImage
           className={styles.logo}
           srcLight="turborepo-dark.svg"
@@ -31,6 +31,8 @@ export default function Home() {
           height={38}
           priority
         />
+      </nav>
+      <main className={styles.main}>
         <ol>
           <li>
             Get started by editing <code>apps/web/app/page.tsx</code>
@@ -47,6 +49,7 @@ export default function Home() {
             href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
             target="_blank"
             rel="noopener noreferrer"
+            aria-label="Deploy to Vercel"
           >
             <Image
               className={styles.logo}
